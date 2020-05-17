613f39b57d1ebdb69edff534de7b3aca62ce258d
Fernando Nascimento <fernando_dev@outlook.com.br>
2020-05-17 12:54:25 -0300

    adicionand a classe modelo e o adaptador da lista

---
+++ app/src/main/java/br/fernando/listadecompras/modelo/Produto.kt
@@ -0,0 +1,17 @@
+package br.fernando.listadecompras.modelo
+
+import android.graphics.Bitmap
+
+/**
+ * classe modelo contendo todos os atributos do produto
+ * a foto pode ser inserida ou pode ficar com a imagem padrao
+ * conforme o usuario achar conveniente
+ * a expressao
+ * bitmap?=null fala que aceita o valor nulo para o atributo foto
+ */
+data class Produto(
+    val nome: String, val quantidade: Int,
+    val valor: Double, val foto: Bitmap?=null
+)
+
+
